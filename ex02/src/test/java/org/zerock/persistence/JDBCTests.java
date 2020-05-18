a4c5eafb4d8ab72786df5dbf1174a1ba33b09c0c
Min <kas4947@gmail.com>
2020-05-19 00:04:59 +0900

    5/18

---
+++ ex02/src/test/java/org/zerock/persistence/JDBCTests.java
@@ -0,0 +1,37 @@
+package org.zerock.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTests {
+
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		Connection con;
+		try {
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "book_ex", "3150");
+			log.info(con);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+}
